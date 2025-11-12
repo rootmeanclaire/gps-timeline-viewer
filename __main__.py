5ac5dd4328ea92c0519b1604d31e1fccd98b707e
Clarity Shimoniak <mail@rootmeanclaire.com>
2025-11-11 18:27:47 -0800

    Generates animation, not sure if it works

--- __main__.py
+++ __main__.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 from sys import argv, stderr
 import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 
 def parse_args():
-	err = False
 	if len(argv) < 2:
 		print("Expected an input CSV file as an argument", file=stderr)
 		exit(1)
-	# Load file
+	return load_csv(argv[1])
+
+def load_csv(filename):
+	err = False
 	print("Loading CSV file...")
-	df = pd.read_csv(argv[1], parse_dates=[0], date_format="%H:%M:%S")
+	df = pd.read_csv(filename, parse_dates=[0], date_format="%H:%M:%S")
 	# Check input file format
 	# Check number of columns
 	if len(df.columns) < 3:
@@ -28,8 +32,26 @@ def parse_args():
 	return df
 
 
+def make_animation(df: pd.DataFrame):
+	# Initialize canvas
+	fig, ax = plt.subplots()
+	pt, = ax.plot([], [], "b.", ms=10)
+	# Animation procedure
+	def draw_frame(idx):
+		pt.set_data([df["Latitude"][idx]], [df["Longitude"][idx]])
+	return FuncAnimation(fig, draw_frame, frames=len(df), interval=20)
+
+
 def main():
-	print(parse_args())
+	df = parse_args()
+	print(df)
+	anim = make_animation(df)
+	print("Saving animation to file...")
+	anim.save("output.mp4")
+	print("Done!")
+	print("Displaying animation...")
+	plt.show()
+	print("Done!")
 
 
 if __name__ == "__main__":
