df2645e557a9e0b4d8c9737c3040f14272413bd8
Clarity Shimoniak <mail@rootmeanclaire.com>
2025-11-15 20:16:30 -0800

    Vertical restack, more flexible plot extents
    Hacky dimensions for new background image

--- animate.py
+++ animate.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 from matplotlib.animation import FuncAnimation
 from PIL import Image
 
@@ -10,11 +11,10 @@ Y_KEY = "Latitude"
 ZOOM_START = datetime.strptime("0:55:26", "%H:%M:%S")
 ZOOM_END = datetime.strptime("01:08:43", "%H:%M:%S")
 SPEED_MULTIPLIER = 10
-LINE_WIDTH = 2
 LINE_COLOR = "red"
 
 
-def set_bounds(ax, x, y):
+def set_bounds(ax, x, y, xslack=0.1, yslack=0.1):
 	xmin = min(x)
 	xmax = max(x)
 	xrange = xmax - xmin
@@ -22,10 +22,10 @@ def set_bounds(ax, x, y):
 	ymax = max(y)
 	yrange = ymax - ymin
 
-	left = xmin - xrange*0.1
-	right = xmax + xrange*0.1
-	bottom = ymin - yrange*0.1
-	top = ymax + yrange*0.1
+	left = xmin - xrange*xslack
+	right = xmax + xrange*xslack
+	bottom = ymin - yrange*yslack
+	top = ymax + yrange*yslack
 
 	ax.set(
 		xlim=[left, right], ylim=[bottom, top],
@@ -39,29 +39,30 @@ def set_bounds(ax, x, y):
 
 def make_animation(df: pd.DataFrame):
 	# Initialize canvas
-	fig = plt.figure(figsize=(12, 6), dpi=96)
-	ax1 = plt.subplot(1, 2, 1)
-	ax2 = plt.subplot(1, 2, 2)
+	fig = plt.figure(figsize=(9, 16), dpi=96)
+	gs = gridspec.GridSpec(2, 1, height_ratios=[1, 2])
+	ax1 = plt.subplot(gs[0])
+	ax2 = plt.subplot(gs[1])
 	ax1.ticklabel_format(style="plain", useOffset=False)
 	ax2.ticklabel_format(style="plain", useOffset=False)
 	zoom_idx = (df["Time"] >= ZOOM_START) & (df["Time"] <= ZOOM_END) & (df["Walking"])
 	# Full Bounds
 	ex1 = set_bounds(ax1, df[X_KEY], df[Y_KEY])
-	ex2 = set_bounds(ax2, df[X_KEY][zoom_idx], df[Y_KEY][zoom_idx])
+	ex2 = set_bounds(ax2, df[X_KEY][zoom_idx], df[Y_KEY][zoom_idx], xslack=5, yslack=3)
 	bg1 = Image.open("bg1.png")
-	bg2 = Image.open("bg2.png")
+	bg2 = Image.open("bg2v2.png")
 	ax1.imshow(bg1, extent=ex1)
-	ax2.imshow(bg2, extent=ex2)
+	ax2.imshow(bg2, extent=[-117.858987 - ((-117.858987) - (-117.860776)) * (1234/781), -117.858987, 33.641766, 33.645278])
 	# Animation objects
 	title1 = ax1.set_title(df["Time"][0])
 	title2 = ax2.set_title(df["Time"][0])
 	path1 = ax1.plot(
 		df[X_KEY][0], df[Y_KEY][0],
-		linewidth=LINE_WIDTH, color=LINE_COLOR
+		linewidth=2, color=LINE_COLOR
 	)[0]
 	path2 = ax2.plot(
 		df[X_KEY][0], df[Y_KEY][0],
-		linewidth=LINE_WIDTH, color=LINE_COLOR
+		linewidth=1, color=LINE_COLOR
 	)[0]
 	# Animation procedure
 	def frame(idx):
